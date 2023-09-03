c09f51abf096fdc18ec5d9696e6ded669af3a15b
annastudio <my_vlan@mail.ru>
2023-09-03 21:50:27 +0300

    Add films

--- app/src/main/java/com/vodoleylan/studio/MainActivity.kt
+++ app/src/main/java/com/vodoleylan/studio/MainActivity.kt
@@ -6,6 +6,49 @@ import androidx.appcompat.app.AppCompatActivity
 import com.vodoleylan.studio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val filmsDataBase = listOf(
+        Film(
+            "Ведьмак",
+            R.drawable.shawshank,
+            "Геральд из Ривии, один из последних ведьмаков. Он охотится на чудовищ, а его неимоверные способности помогают ему в этом."
+        ),
+        Film(
+            "The Godfather",
+            R.drawable.god_father,
+            "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."
+        ),
+        Film(
+            "The Dark Knight",
+            R.drawable.dark_knight,
+            "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice."
+        ),
+        Film(
+            "Pulp Fiction",
+            R.drawable.pulp,
+            "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption."
+        ),
+        Film(
+            "Inception",
+            R.drawable.inception,
+            "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O."
+        ),
+        Film(
+            "Hamilton",
+            R.drawable.hamilton,
+            "The real life of one of America's foremost founding fathers and first Secretary of the Treasury, Alexander Hamilton. Captured live on Broadway from the Richard Rodgers Theater with the original Broadway cast."
+        ),
+         Film(
+            "Interstellar",
+            R.drawable.interstellar,
+            "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival."
+        ),
+        Film(
+            "Joker",
+            R.drawable.joker,
+            "In Gotham City, mentally troubled comedian Arthur Fleck is disregarded and mistreated by society. He then embarks on a downward spiral of revolution and bloody crime. This path brings him face-to-face with his alter-ego: the Joker."
+        ),
+    )
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +68,7 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
                     true
                 }
+
                 else -> false
             }
         }
@@ -36,14 +80,17 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
                     true
                 }
+
                 R.id.watch_later -> {
                     Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
                     true
                 }
+
                 R.id.selections -> {
                     Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
                     true
                 }
+
                 else -> false
             }
         }
