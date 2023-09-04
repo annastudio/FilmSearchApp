b539b7f8e30cd870e1acae59a01d993e1545e9b7
annastudio <my_vlan@mail.ru>
2023-09-05 00:25:31 +0300

    Add Fragment

---
+++ app/src/main/java/com/vodoleylan/studio/DetailsFragment.kt
@@ -0,0 +1,39 @@
+package com.vodoleylan.studio
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.vodoleylan.studio.databinding.FragmentDetailsBinding
+
+class DetailsFragment : Fragment() {
+    private lateinit var binding: FragmentDetailsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        //return inflater.inflate(R.layout.fragment_details, container, false)
+        binding = FragmentDetailsBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setFilmsDetails()
+    }
+
+    private fun setFilmsDetails() {
+        //Получаем наш фильм из переданного бандла
+        val film = arguments?.get("film") as Film
+
+        //Устанавливаем заголовок
+        binding.detailsToolbar.title = film.title
+        //Устанавливаем картинку
+        binding.detailsPoster.setImageResource(film.poster)
+        //Устанавливаем описание
+        binding.detailsDescription.text = film.description
+    }
+}
