35983a2d7c513fdd780f7809d23b28fa1ce0b92a
annastudio <my_vlan@mail.ru>
2023-09-05 02:39:29 +0300

    Add setFavorites and shareFilm

--- app/src/main/java/com/vodoleylan/studio/DetailsFragment.kt
+++ app/src/main/java/com/vodoleylan/studio/DetailsFragment.kt
@@ -1,5 +1,6 @@
 package com.vodoleylan.studio
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,8 @@ import com.vodoleylan.studio.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment() {
     private lateinit var binding: FragmentDetailsBinding
+
+    private lateinit var film: Film
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +26,9 @@ class DetailsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setFilmsDetails()
+
+        binding.detailsFabFavorites.setOnClickListener { setFavorites() }
+        binding.detailsFabShare.setOnClickListener { shareFilm() }
     }
 
     private fun setFilmsDetails() {
@@ -35,5 +41,34 @@ class DetailsFragment : Fragment() {
         binding.detailsPoster.setImageResource(film.poster)
         //Устанавливаем описание
         binding.detailsDescription.text = film.description
+    }
+
+    private fun setFavorites() {
+        val film = arguments?.get("film") as Film
+
+        if (!film.isInFavorites) {
+            binding.detailsFabFavorites.setImageResource(R.drawable.ic_baseline_star)
+            film.isInFavorites = true
+        } else {
+            binding.detailsFabFavorites.setImageResource(R.drawable.ic_baseline_star_border)
+            film.isInFavorites = false
+        }
+    }
+
+    private fun shareFilm() {
+        val film = arguments?.get("film") as Film
+        //Создаем интент
+        val intent = Intent()
+        //Указываем action с которым он запускается
+        intent.action = Intent.ACTION_SEND
+        //Кладем данные о нашем фильме
+        intent.putExtra(
+            Intent.EXTRA_TEXT,
+            "Check out this film: ${film.title} \n\n ${film.description}"
+        )
+        //Указываем MIME тип, чтобы система знала, какое приложения предложить
+        intent.type = "text/plain"
+        //Запускаем наше активити
+        startActivity(Intent.createChooser(intent, "Share To:"))
     }
 }
